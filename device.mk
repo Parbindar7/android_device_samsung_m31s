710da316431e4c263e68b82bd4ff6525c671fa47
FelcoX <godxyukarzzyt@gmail.com>
2026-04-13 14:53:18 +0000

    m31s: drop android.hardware.nfc-service.sec

--- device.mk
+++ device.mk
@@ -47,10 +47,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
      $(DEVICE_PATH)/configs/keylayout/uinput-sec-fp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-sec-fp.kl
 
-# NFC
-PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.sec
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf \
     $(DEVICE_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
